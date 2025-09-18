ee63d9a4eff3c97db05eec58a6310b60aebae6dd
kentatech <logankenta254@gmail.com>
2025-09-19 00:49:30 +0300

    added sentry library for monitoring and troubleshooting application + Slack

--- main.py
+++ main.py
@@ -1,20 +1,63 @@
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import datetime
+import sentry_sdk
+
+# Initialize Sentry for error tracking and performance monitoring
+sentry_sdk.init(
+    dsn="https://b9209a4599b4a4a133ee86ab3e929af9@o4510040530550784.ingest.de.sentry.io/4510040591892560",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Enable sending logs to Sentry
+    enable_logs=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
+)
+
+# Send logs directly to Sentry
+# sentry_sdk.logger.info('This is an info log message')
+# sentry_sdk.logger.warning('This is a warning message')
+# sentry_sdk.logger.error('This is an error message')
 
 app = Flask(__name__)
 
 jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this to a random secret key in production
 
+
+
 products_list = []
 sales_list = []
 purchases_list = []
 users_list = []
 
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except(ValueError, TypeError):
+        return False
+    
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except(ValueError, TypeError):
+        return False
+
 @app.route("/",methods=['GET'])
 def home():
-    res = {"flask api version": "1.0.0"}
-    return jsonify(res), 200
+     res = {"flask api version": "1.0.0"}
+     return jsonify(res), 200
 
 @app.route("/api/register", methods=["POST"])
 def register():
@@ -91,26 +134,26 @@ def purchases():
     if request.method == "GET":
         return jsonify(purchases_list), 200
     
-    if request.method == "POST":
+    elif request.method == "POST":
         data = request.get_json()
         if not data:
             return jsonify({"Error": "Request must be in JSON"}), 400
-            if "product_id" not in data or "quantity" not in data:
-                return jsonify({"error": "Ensure all fields are set: product_id, quantity"}), 400
-            elif is_int(data[product_id]):
-                return jsonify({"error":"product_id must be an int"}), 400
-            if not is_number(data["quantity"]):
-                return jsonify({"error":"Quantity Must be a number"}), 400
-            else:
-                purchase = {
-                    "product_id": int(data["product_id"]),
-                    "quantity": float(data["quantity"]),
-                    "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                }
+        if "product_id" not in data or "quantity" not in data:
+            return jsonify({"error": "Ensure all fields are set: product_id, quantity"}), 400
+        elif not is_int(data["product_id"]):
+            return jsonify({"error":"product_id must be an int"}), 400
+        if not is_number(data["quantity"]):
+            return jsonify({"error":"Quantity Must be a number"}), 400
+        else:
+            purchase = {
+                "product_id": int(data["product_id"]),
+                "quantity": float(data["quantity"]),
+                "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
             purchases_list.append(purchase)
             return jsonify(purchase), 201
-        else:
-            return jsonify({"error": "Method not allowed"}), 405
+    else:
+        return jsonify({"error": "Method not allowed"}), 405
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -130,4 +173,6 @@ if __name__ == "__main__":
 # A  Client(web,mobile) can not access a protected route without a token
 # The client stores that toejn once they login or register
 # pip install flask-jwt-extended
-#
+#Install slack + sentry integration
+#Create a sentry account and a project
+#Integrate sentry with flask    
