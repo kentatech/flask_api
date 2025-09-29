869da5fec46dfebef9a410705c00c29222b18d64
kentatech <logankenta254@gmail.com>
2025-09-29 08:06:47 +0300

    base configs is off until proper setup

--- configs/base_configs.py
+++ configs/base_configs.py
@@ -1,10 +1,10 @@
-# import secrets
-# import os
-# class Base:
-#     SECRET_KEY = secrets.token_hex(16)
-# class Development(Base):
-#     FLASK_APP = os.environ.get("main.py")
-#     FLASK_ENV= os.environ.get("development")
-#     POSTGRES_USER = os.environ.get("POSTGRES")
-#     POSTGRES_PASSWORD = os.environ.get("NEWPAS4u.")
-#     POSTGRES_DB = os.environ.get("flask_api")
+import secrets
+import os
+class Base:
+    SECRET_KEY = secrets.token_hex(16)
+class Development(Base):
+    FLASK_APP = os.environ.get("main.py")
+    FLASK_ENV= os.environ.get("development")
+    POSTGRES_USER = os.environ.get("POSTGRES")
+    POSTGRES_PASSWORD = os.environ.get("NEWPAS4u.")
+    POSTGRES_DB = os.environ.get("flask_api")
