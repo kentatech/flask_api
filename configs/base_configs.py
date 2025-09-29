5f29025cd2af2af9ca5f2e45039f5461c2f00f35
kentatech <logankenta254@gmail.com>
2025-09-29 07:33:43 +0300

    base config added db

--- configs/base_configs.py
+++ configs/base_configs.py
@@ -4,8 +4,7 @@ class Base:
     SECRET_KEY = secrets.token_hex(16)
 class Development(Base):
     FLASK_APP = os.environ.get("main.py")
-    FLASK_ENV= os.environ.get("FLASK_ENV")
-    DATABASE = os.environ.get("flask_api")
+    FLASK_ENV= os.environ.get("development")
     POSTGRES_USER = os.environ.get("POSTGRES")
     POSTGRES_PASSWORD = os.environ.get("NEWPAS4u.")
     POSTGRES_DB = os.environ.get("flask_api")
