ed4d5baf9c5c963afc19fdc31a9831c4866822f8
kentatech <logankenta254@gmail.com>
2025-09-30 23:26:30 +0300

    testing configs

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class Sale(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     quantity = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime)
     product = db.relationship('Product', backref=db.backref('sales', lazy=True))
 
 class Purchase(db.Model):
